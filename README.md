5e96632f77305c070f382ac745f8477fb5ab2828
limit <minyi.lin@frontpay.cn>
2016-10-14 15:44:36 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # gulp-tmpl
-A project of Static template
+纯静态可集静态数据的模板
+
+## 下载项目
+```
+git clone  https://github.com/limitLam/gulp-tmpl.git
+```
+
+## 安装node插件
+```
+切换到cnpm速度更快一些
+
+npm config set registry https://registry.npm.taobao.org
+
+npm install
+```
+
+## 启动项目
+```
+npm start
+```
+
+## 目录结构
+```
+config						#配置方法
+	|--getIPAddress.js		#获取本地IP的方法
+	|--utils.js				#一些特殊的公用方法
+nunjucks					#静态模板nunjucks调用的静态数据
+	|--base.json			#基本配置数据
+	|--catalogs.json		#分类数据
+	|--items.json			#商品数据
+	|--profiles.json		#文案数据
+src 						#项目源码目录
+	|--static				#静态资源目录
+		|--css				#样式目录
+			|--lib			#不更换版本号的样式资源目录(如bootstrap.css)
+		|--fonts			#字体目录
+		|--images			#图片目录
+		|--js				#脚本目录
+			|--lib			#不更换版本号的脚本资源目录(如bootstrap.js,jquery.js)
+	|--template				#模板目录
+		|--_components		#公用组件模板目录
+		|--_layout			#整体布局模板目录
+		|--catalog			#分类模板目录
+		|--index			#首页模板目录
+		|--item				#商品模板目录
+		|--profile			#文案模板目录
+tasks						#gulp任务目录
+.gitignore 					#git的配置文件，配置不上传到git的文件
+config.json 				#基础配置数据文件
+gulpfile.js 				#gulp配置文件
+package.json 				#项目信息文件，包括npm包
+README.md 					#markdown说明文档
+
+```
--- tasks/template.js
+++ tasks/template.js
@@ -9,12 +9,41 @@ const getJson = utils.getJson;
 gulp.task('template', function() {
     const TmplBaseConfig = getJson('nunjucks/base.json'); //基础数据，版本号等
 
+    //  以下考虑可读性问题，暂时不改造
+    let getBaseData = function(){
+        return Object.assign({},TmplBaseConfig);
+    }
+    //  catalog
+    let catalogData = getJson('nunjucks/catalogs.json');
+    const TmplCatalogs = catalogData.catalogs;
+
+    //  遍历catalogs
+    TmplCatalogs.map(function(catalog, index){
+        function getData() {
+            var baseData = getBaseData();
+            return Object.assign(baseData, catalog);
+        }
+        gulp.src([config.template + '/catalog/_index.html'])
+            .pipe($.data(getData)) //  数据合并
+            .pipe($.nunjucksRender({
+                path: [config.template]
+            }))
+            .pipe($.prettify({
+                indent_size: 2
+            }))
+            .pipe($.rename('catalog/' + catalog.name + '.html'))
+            .pipe(gulp.dest(config.destPath))
+    });
+
+    //  item
     let itemData = getJson('nunjucks/items.json');
     const TmplItems = itemData.items;
+
     // 遍历items
     TmplItems.map(function(item, index) {
         function getData() {
-            return Object.assign({}, TmplBaseConfig, item);
+            var baseData = getBaseData();
+            return Object.assign(baseData, item);
         }
         gulp.src([config.template + '/item/_index.html'])
             .pipe($.data(getData)) //  数据合并
@@ -24,10 +53,33 @@ gulp.task('template', function() {
             .pipe($.prettify({
                 indent_size: 2
             }))
-            .pipe($.rename('item/' + item.id + '.html'))
+            .pipe($.rename('item/' + item.class + '/' + item.name + '.html'))
             .pipe(gulp.dest(config.destPath))
     });
 
+    //  profile
+    let profileData = getJson('nunjucks/profiles.json');
+    const TmpProfiles = profileData.profiles;
+
+    // 遍历items
+    TmpProfiles.map(function(profile, index) {
+        function getData() {
+            var baseData = getBaseData();
+            return Object.assign(baseData, profile);
+        }
+        gulp.src([config.template + '/profile/_index.html'])
+            .pipe($.data(getData)) //  数据合并
+            .pipe($.nunjucksRender({
+                path: [config.template]
+            }))
+            .pipe($.prettify({
+                indent_size: 2
+            }))
+            .pipe($.rename('profile/' + profile.id + '.html'))
+            .pipe(gulp.dest(config.destPath))
+    });
+
+    //  其他无数据页面
     return gulp.src([config.template + '/**/**.html', '!' + config.template + '/**/_**.html', '!' + config.template + '/_**/*.html'])
         .pipe($.nunjucksRender({
             path: [config.template],
